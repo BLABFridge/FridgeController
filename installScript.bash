799b7e8d81ed03236a2520e806d1c5660c5e71db
packocrayons <gibson.brydon@gmail.com>
2017-03-06 06:07:27 -0500

    removed some leftovers

--- installScript.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Write the current directory as the directory in the minicom dfl file, this is where minicom should log the data
-echo "pu downdir $(pwd)" > minirc.dfl.requiredConfig
-#move the minicom file to the correct directory, this requiredConfig file should not be platform dependant (untested)
-mv ./minirc.dfl.requiredConfig /etc/minicom/minirc.dfl
-
-#Do any more install stuff here
